baf82961f4da0cbcac1b7ab77e62628c9d7152db
Tony Rivera <ajboricua@gmail.com>
2021-08-05 16:04:22 -0400

    .

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require("inquirer");
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
 
-console.log(profileDataArgs);
+{
+    type: 'input',
+    name: 'name',
+    message: 'what is your name? (Required)',
+    validate: nameINput -> {
+        if (nameInput) {
+            return true;
+        }esle {
+            console.log('Pleease type your name!');
+            return false;
+        }
+    }
+},
 
-const [name, github] = profileDataArgs;
 
-console.log(name, github);
 
-const pageHTML = generatePage(name, github);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+// const profileDataArgs = process.argv.slice(2);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// console.log(profileDataArgs);
+
+// const [name, github] = profileDataArgs;
+
+// console.log(name, github);
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
